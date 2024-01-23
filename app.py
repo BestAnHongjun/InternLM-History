050c621629bc96a654f3ace0e1f0ab19d3e49f64
Coder.AN <an.hongjun@foxmail.com>
2024-01-23 15:56:32 +0800

    update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 
 def greet(name):
-    return "Hello" + name + "!!"
+    return "您的输入是：" + name + "。这只是一个测试Demo，项目仍在开发中。"
 
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
