5b34947d0be8aa47f9ff17c4b19d87449557a54d
Coder.AN <an.hongjun@foxmail.com>
2024-01-24 10:55:21 +0800

    update app

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 # setup environments
 import os 
-os.system("./setup.sh")
-os.environ["no_proxy"] = "localhost,127.0.0.1,::1"
+if not os.path.exists("flash-attention"):
+    os.system("./setup.sh")
 
 import gradio as gr
+from openxlab.model import download
+    
+
+def download_model():
+    download(model_repo='Coder-AN/InternLM-History', output='model/internlm-chat-7b-history')
 
 
 def greet(name):
     return "您的输入是：" + name + "。这只是一个测试Demo，项目仍在开发中。"
 
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch(share=True)
+if __name__ == "__main__":
+    os.environ["no_proxy"] = "localhost,127.0.0.1,::1"
+    download_model()
+    iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+    iface.launch(share=True)
