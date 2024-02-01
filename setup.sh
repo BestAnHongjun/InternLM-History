0250105b1f8f86c4a947aaa17553fc7bb1ece594
Coder.AN <an.hongjun@foxmail.com>
2024-02-01 20:19:37 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,15 @@ pip install torch==2.0.1
 pip install xtuner[deepspeed]
 
 # MAX_JOBS=4 pip install flash-attn --no-build-isolation
-git clone https://gitee.com/an_hongjun/flash-attention.git
+mkdir flash-attention
 cd flash-attention
-MAX_JOBS=4 python setup.py install
+wget -N https://github.com/Dao-AILab/flash-attention/releases/download/v2.3.5/flash_attn-2.3.5+cu117torch2.0cxx11abiTRUE-cp39-cp39-linux_x86_64.whl
+pip install flash_attn-2.3.5+cu117torch2.0cxx11abiTRUE-cp39-cp39-linux_x86_64.whl
 cd ..
+# git clone https://gitee.com/an_hongjun/flash-attention.git
+# cd flash-attention
+# MAX_JOBS=4 python setup.py install
+# cd ..
 
 pip install lmdeploy[all]==v0.1.0
 pip install gradio==4.15.0
